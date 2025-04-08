9f7dd155b986d525f6fe8126d549ad6d39472b4f
yg.choi <yg.choi@himec.co.kr>
2025-04-08 17:00:26 +0900

    style 다루기 내용 추가 그리고 셀프체크

---
+++ 2nd-week/ch03-self-check/src/App.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  data() {
+    return {
+      number: 0,
+    };
+  },
+  computed: {
+    formatNumber: function() {
+      let str = this.number.toString();
+      if (this.number < 10) {
+        str = "0" + str;
+      }
+
+      return str;
+    },
+  },
+  watch: {
+    number(newValue, oldValue) {
+      if (newValue > 10) {
+        console.log("10을 초과했습니다.");
+        this.number = 10;
+      } else if (newValue < 0) {
+        console.log("0 미만입니다.");
+        this.number = 0;
+      }
+    },
+  },
+};
+</script>
+<template>
+  {{ formatNumber }}&nbsp;
+  <button v-on:click="number++">증가</button>&nbsp;
+  <button v-on:click="number--">감소</button>&nbsp;
+  <button type="reset" v-on:click="number = 0">초기화</button>&nbsp;
+</template>
+<style></style>
---
+++ 2nd-week/ch03-style/src/App.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  data() {
+    return {
+      primaryColor: 'blue',
+      primaryWeight: 'bold',
+      primaryDecoration: 'underline',
+      // primaryStyle: 'italic', // fontStyle: italic, normal, oblique 등
+    };
+  },
+}
+</script>
+<template>
+  <h1 v-bind:style="{ color: primaryColor, fontWeight: primaryWeight, textDecoration: primaryDecoration }">Inline Style</h1>
+  <h1 class="test">External Style</h1>
+</template>
+<style>
+  /* h1 {
+    color: red;
+    font-style: italic;
+  } */
+  @import '~/main.css';
+</style>
